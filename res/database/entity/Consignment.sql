f7747fd0bcfcf7f3744540f23e10f5f0890d249f
Joseph <joseph@captaincourier.org>
2013-09-11 11:35:10 +0000

    updated Consignment table to reference both Address and Contact tables. Added indexes to all foreign keys in Consignment

--- res/database/entity/Consignment.sql
+++ res/database/entity/Consignment.sql
@@ -15,14 +15,26 @@ CACHE 1;
 
 CREATE TABLE "Consignment" (
 	id int DEFAULT nextval('"seq_Consignment_id"' :: regclass),
-	"collectionAddress" int,
-	"collectionContact" int,
+	"collectionAddressId" int NOT NULL,
+	"collectionContactId" int NOT NULL,
 	"collectionTime" timestamp NOT NULL,
-	"deliveryAddress" int,
-	"deliveryContact" int,
+	"deliveryAddressId" int NOT NULL,
+	"deliveryContactId" int NOT NULL,
 	"deliveryTime" timestamp NOT NULL,
 	"contractNumber" citext NOT NULL,
 	"serviceCode" citext NOT NULL,
-	CONSTRAINT "pk_Consignment" PRIMARY KEY (id)
+	CONSTRAINT "pk_Consignment" PRIMARY KEY (id),
+	CONSTRAINT "fk_Consignment_collectionAddressId" FOREIGN KEY ("collectionAddressId") REFERENCES "Address" ("id"),
+	CONSTRAINT "fk_Consignment_collectionContactId" FOREIGN KEY ("collectionContactId") REFERENCES "Contact" ("id"),
+	CONSTRAINT "fk_Consignment_deliveryAddressId" FOREIGN KEY ("deliveryAddressId") REFERENCES "Address" ("id"),
+	CONSTRAINT "fk_Consignment_deliveryContactId" FOREIGN KEY ("deliveryContactId") REFERENCES "Contact" ("id")
 );
 
+ALTER SEQUENCE "seq_Consignment_id" OWNED BY "Consignment".id;
+
+CREATE INDEX "idx_Consignment_collectionAddressId" ON "Consignment" USING BTREE("collectionAddressId");
+CREATE INDEX "idx_Consignment_collectionContactId" ON "Consignment" USING BTREE("collectionContactId");
+CREATE INDEX "idx_Consignment_deliveryAddressId" ON "Consignment" USING BTREE("deliveryAddressId");
+CREATE INDEX "idx_Consignment_deliveryContactId" ON "Consignment" USING BTREE("deliveryContactId");
+
+CREATE INDEX "idx_Consignment_contractNumber" ON "Consignment" USING BTREE("contractNumber");
