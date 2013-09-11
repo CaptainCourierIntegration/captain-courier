965c3c8c982ac5ec81c532ce27b9b971f223d7c5
Joseph <joseph@captaincourier.org>
2013-09-11 13:49:54 +0000

    added SQL Authentication Table

---
+++ res/database/entity/admin/Authentication.sql
@@ -0,0 +1,23 @@
+/** resolver
+{
+    "depends": ["captain", "hash"],
+    "searchPath": "captain"
+}
+*/
+
+CREATE SEQUENCE "seq_Authentication_id"
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+CREATE TABLE "Authentication" (
+    "id" int NOT NULL DEFAULT nextval('"seq_Authentication_id"'::regclass),
+	"hashType" hash NOT NULL DEFAULT 'SHA1',
+	"hash" text NOT NULL,
+	"salt" text NOT NULL,
+    CONSTRAINT "pk_Authentication" PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE "seq_Authentication_id" OWNED BY "Authentication"."id";
