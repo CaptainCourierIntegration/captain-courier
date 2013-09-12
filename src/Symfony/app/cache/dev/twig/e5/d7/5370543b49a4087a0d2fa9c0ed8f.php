685e85afc017aca709e683289088c55aaae998e4
Joseph <joseph@captaincourier.org>
2013-09-12 15:23:58 +0000

    added CaptainCourierBundle

---
+++ src/Symfony/app/cache/dev/twig/e5/d7/5370543b49a4087a0d2fa9c0ed8f.php
@@ -0,0 +1,39 @@
+<?php
+
+/* CaptainCourierCaptainBundle:Default:index.html.twig */
+class __TwigTemplate_e5d75370543b49a4087a0d2fa9c0ed8f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CaptainCourierCaptainBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
