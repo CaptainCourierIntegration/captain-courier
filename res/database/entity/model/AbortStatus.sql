ecee03746283fccecdc4dee112cbfc515b7b6190
Joseph <joseph@captaincourier.org>
2013-09-12 10:26:04 +0000

    added SQL table AbortStatus

--- res/database/entity/model/AbortStatus.sql
+++ res/database/entity/model/AbortStatus.sql
@@ -1,12 +1,25 @@
 /** resolver
 {
+	"depends": ["captain", "abortstatus_status", "citext"],
+	"searchPath": "captain, extension"
 }
 */
 
 
-CREATE SEQUENCE 
+CREATE SEQUENCE "seq_AbortStatus_id"
+	START WITH 1
+	INCREMENT BY 1
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1
 
 CREATE TABLE "AbortStatus" (
 	"id" int NOT NULL,
-	
+	"shipmentId" int NOT NULL,
+	"status" abortstatus_status NOT NULL,
+	"message" citext,
+	CONSTRAINT "pk_AbortStatus" PRIMARY KEY ("id"),
+	CONSTRAINT "fk_AbortStatus_shipmentId" FOREIGN KEY ("shipmentId") REFERENCES "Shipment" ("id")
 );
+
+ALTER SEQUENCE "seq_AbortStatus_id" OWNED BY "AbortStatus"."id";
