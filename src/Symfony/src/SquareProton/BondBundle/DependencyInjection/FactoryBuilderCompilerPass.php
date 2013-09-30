0aa0e69c8ae84d89b9fabf7f9a9dc01e582d3672
Joseph <joseph@captaincourier.org>
2013-09-30 16:38:02 +0000

    created new bundle to integrate Bond with Symfony

---
+++ src/Symfony/src/SquareProton/BondBundle/DependencyInjection/FactoryBuilderCompilerPass.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SquareProton\BondBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Definition;
+
+use Bond\Di\FactoryBuilderExtension;
+use Bond\Di\Inject;
+class FactoryBuilderCompilerPass implements CompilerPassInterface
+{
+
+	const TAG = "factory";
+
+	public function process(ContainerBuilder $container)
+	{
+		$services = $container->findTaggedServiceIds(self::TAG);
+
+		foreach ($services as $serviceName => $tagConfigs) {
+			$def = $container->getDefinition($serviceName);
+			d(new FactoryMapper($def));
+		}	
+		// $config = [];
+		// foreach ($services as $serviceName => $tagConfigs) {
+		// 	foreach ($tagConfigs as $tagConfig) {
+		// 		if(count($tagConfig) > 1 or (count($tagConfig) == 1 and !isset($tagConfig["definition"]))) {
+		// 			throw new \Exception(sprintf("%s tag has the optional parameter 'definition', no other parameters allowed", self::TAG));
+		// 		}
+		// 		$factoryName = isset($tagConfig["definition"]) ? $tagConfig["definition"] : "{$serviceName}Factory";
+		// 		$config[$factoryName] = $serviceName;
+		// 	}
+		// }
+
+		// $factoryBuilder = new FactoryBuilderExtension();
+		// $factoryBuilder->load([$config], $container);
+	}
+
+	private function resolveReferences(ContainerBuilder $container, Definition $definition)
+	{
+		$args = [];
+		foreach($definition->getArguments() as $argument) {
+			if($argument instanceof Definition) {
+				$args[] = $this->resolveReferences($container, $argument);
+			} else if($argument instanceof Reference) {
+				$args[] = $this->resolveReferences($container, $container->get((string)$argument));
+			} 
+		}
+	}
+
+}
--- src/Symfony/src/SquareProton/BondBundle/SquareProtonBondBundle.php
+++ src/Symfony/src/SquareProton/BondBundle/SquareProtonBondBundle.php
@@ -2,8 +2,19 @@
 
 namespace SquareProton\BondBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Bond\Di\FactoryBuilderExtension;
+
+use SquareProton\BondBundle\DependencyInjection\FactoryBuilderCompilerPass;
 
 class SquareProtonBondBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+	{
+//        $factoryBuilderExtension = new FactoryBuilderExtension();
+//        $container->registerExtension($factoryBuilderExtension);
+
+		$container->addCompilerPass(new FactoryBuilderCompilerPass());
+	}
 }
