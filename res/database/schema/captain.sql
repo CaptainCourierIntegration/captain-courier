4fa6a175f2766488c701742da50d48746311acba
Joseph <joseph@captaincourier.org>
2013-09-30 16:38:17 +0000

    tweak

--- res/database/schema/captain.sql
+++ res/database/schema/captain.sql
@@ -1,10 +1,20 @@
 /** resolver
 {
-    "depends": [],
-    "searchPath": "common"
+    "depends": [
+        "common",
+        "extension"
+    ]
 }
 */
 
-
-
 CREATE SCHEMA "captain";
+
+-- anonymous code block to get around the failure to alter the 'current database'
+-- ANSI standard interval style for compatability with Php. Think Bond\Entity\Types\Interval
+DO language plpgsql $$
+DECLARE
+    db text := quote_ident( current_database() );
+BEGIN
+    EXECUTE 'ALTER DATABASE ' || db || ' SET search_path TO captain, common, extension;';
+END;
+$$;
