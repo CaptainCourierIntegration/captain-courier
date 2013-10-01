21b70a5580b7fff38e04e0777968597086990c8f
Joseph <joseph@captaincourier.org>
2013-10-01 16:59:51 +0100

    added license header

--- src/Symfony/src/CaptainCourier/CaptainBundle/Controller/AddressController.php
+++ src/Symfony/src/CaptainCourier/CaptainBundle/Controller/AddressController.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Captain Courier Integration <captain@captaincourier.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CaptainCourier\CaptainBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
