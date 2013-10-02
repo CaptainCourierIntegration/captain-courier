9eccad275d9108a7b89a33fd368a4179f3d9fd33
Joseph <joseph@captaincourier.org>
2013-10-02 16:46:01 +0100

    added cidr service to container and made shipmentController dependenant on it

--- src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ShipmentController.php
+++ src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ShipmentController.php
@@ -17,23 +17,25 @@ class ShipmentController extends RestController
 	private $d;
 	private $entityManager;
 	private $database;
+	private $cidr;
 
-	public function __construct($d, $entityManager, $database)
+	public function __construct($d, $entityManager, $database, $cidr)
 	{
 		parent::__construct();
 		$this->d = $d;
 		$this->entityManager = $entityManager;
 		$this->database = $database;
+		$this->cidr = $cidr;
 	}
 
 	/**
 	 *
 	 *
 	 * REQUESET
-	 *   - toId
-	 *   - fromId
-	 *   - parcelId
-	 *   - itemsIds
+	 *   - *to
+	 *   - *from
+	 *   - *parcel
+	 *   - *items
 	 * 
 	 * RESPONSE
 	 *   - id
@@ -46,6 +48,14 @@ class ShipmentController extends RestController
 	public function createShipmentAction()
 	{
 		$data = json_decode($this->get("request")->getContent());
+
+		$formattedData = [
+			"collectionAddressId" => $this->entityManager["Address"]->find($data->from),
+			"deliveryAddressId" => $this->entityManager["Address"]->find($data->to),
+			"parcelId" => $this->entityManager["Parcel"]->find($data->parcel)
+		];
+
+
 
 		return new Response(
 			json_encode($data),
