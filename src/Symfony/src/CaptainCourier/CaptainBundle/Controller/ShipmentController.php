a19d4ca27ae0a8e98d2724856fcc42dc440e257a
Joseph <joseph@captaincourier.org>
2013-10-02 12:36:30 +0100

    added ShipmentController createShipment action and its associated routing. also made ShipmentController a service

--- src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ShipmentController.php
+++ src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ShipmentController.php
@@ -13,54 +13,45 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ShipmentController extends RestController
 {
-	private $response;
 
-	public function __construct()
+	private $d;
+	private $entityManager;
+	private $database;
+
+	public function __construct($d, $entityManager, $database)
 	{
 		parent::__construct();
-		$this->response = new Response(
-			'{"status": "ALL GOOD"}',
+		$this->d = $d;
+		$this->entityManager = $entityManager;
+		$this->database = $database;
+	}
+
+	/**
+	 *
+	 *
+	 * REQUESET
+	 *   - toId
+	 *   - fromId
+	 *   - parcelId
+	 *   - itemsIds
+	 * 
+	 * RESPONSE
+	 *   - id
+	 *   - type: Shipment
+	 *   - toAddress
+	 *   - fromAddress
+	 *   - parcel
+	 *   - items
+	 */
+	public function createShipmentAction()
+	{
+		$data = json_decode($this->get("request")->getContent());
+
+		return new Response(
+			json_encode($data),
 			200,
-			array('content-type' => "application/json")
+			array('content-type' => 'application/json')
 		);
-	}
-
-	// view
-	public function cgetAction()
-	{
-		return $this->response;
-	}
-
-	// get
-	public function getAction($id)
-	{
-		return $this->response;
-	}
-
-	// create
-	public function cpostAction()
-	{
-		return $this->response;
-	}
-
-	public function getConfirmAction($id)
-	{
-		return $this->response;
-	}
-
-	public function getAbortAction($id)
-	{
-		return $this->response;
-	}
-
-	public function getTrackAction($id)
-	{
-		return $this->response;
-	}
-
-	public function getQuotesAction($id)
-	{
-		return $this->response;
 	}
 
 }
