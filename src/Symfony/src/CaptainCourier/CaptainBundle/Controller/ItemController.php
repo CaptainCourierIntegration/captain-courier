97748fed6adb7de2b60397236ee97702d6b16f71
Joseph <joseph@captaincourier.org>
2013-10-02 12:07:49 +0100

    implemented the createItem action

--- src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ItemController.php
+++ src/Symfony/src/CaptainCourier/CaptainBundle/Controller/ItemController.php
@@ -10,6 +10,8 @@
 namespace CaptainCourier\CaptainBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Bond\Sql\Query;
+use Bond\Pg\Result;
 
 class ItemController extends RestController
 {
@@ -26,8 +28,63 @@ class ItemController extends RestController
 		$this->database = $database;
 	}
 
+	/**
+	 *
+	 * REQUEST
+	 *   - *description
+	 *   - *quantity
+	 *   - *weight
+	 *   - origin(2digit country code)
+	 *   - hsTarrifNumber
+     *
+	 * RESPONSE
+	 *   - id
+	 *   - type: Item
+	 *   - description
+	 *   - quantity
+	 *   - weight
+	 *   - origin
+	 *   - hsTarrifNumber
+	 */
 	public function createItemAction()
 	{
-		
+		$content = json_decode($this->get("request")->getContent());
+
+		$formattedContent = [
+			"description" => $content->description,
+			"quantity" => $content->quantity,
+			"weight" => $content->weight
+		];
+
+		if (isset($content->origin)) {
+			$formattedContent["originCountryCode"] = $this->entityManager["Country"]->find($content->origin);
+		}
+
+		if (isset($content->hsTarrifNumber)) {
+			$formattedContent["hsTarrifNumber"] = $content->hsTarrifNumber;
+		}
+		d($formattedContent);
+
+		$item = $this->entityManager->getRepository("Item")->make($formattedContent);
+		$this->entityManager->recordManager->persist($item);
+		$this->entityManager->recordManager->flush();
+
+		d($item);
+
+		$formattedResponseData = [
+			"id" => $item->getId(),
+			"type" => "Item",
+			"description" => $item->getDescription(),
+			"quantity" => $item->getQuantity(),
+			"weight" => $item->getWeight(),
+			"origin" => $item->getOriginCountryCode()->getCc(),
+			"hsTarrifNumber" => $item->getHsTarrifNumber()
+		];
+
+		return new Response(
+			json_encode($formattedResponseData),
+			200,
+			array('content-type' => "application/json")
+		);
 	}
 }
